79752fdff1f39babf8a54a8e50d4d5f671d241c3
Clifford <Clifford@DESKTOP.hsd1.nj.comcast.net>
2018-09-25 00:29:11 -0400

    Initial import

---
+++ src/main/java/util/transcription/TranscriptionResultFileParser.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2018 Clifford Errickson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package util.transcription;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.transcription.data.Alternative;
+import util.transcription.data.Result;
+import util.transcription.data.SpeakerLabel;
+import util.transcription.data.Timestamp;
+import util.transcription.data.Transcription;
+
+/**
+ * A utility for parsing a Transcription result file into multiple outputs.
+ */
+public class TranscriptionResultFileParser {
+
+  private final static String OUTPUT1_FILENAME = "continuous_transcript.txt";
+  private final static String OUTPUT2_FILENAME = "speaker_duration.txt";
+  private final static String OUTPUT3_FILENAME = "keyword_count.txt";
+
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Usage: TranscriptionResultFileParser input");
+    }
+
+    TranscriptionResultFileParser parser = new TranscriptionResultFileParser();
+
+    try {
+      parser.parse(args[0]);
+    } catch (IOException ex) {
+      Logger.getLogger(TranscriptionResultFileParser.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  public void parse(String input) throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    Transcription transcription = objectMapper.readValue(new File(input), Transcription.class);
+
+    // Continuous transcript
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT1_FILENAME))) {
+      for (Result result : transcription.getResults()) {
+        writer.write(result.getAlternatives().get(0).getTranscript());
+      }
+    }
+
+    // speaker duration
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT2_FILENAME))) {
+      SpeakerLabel startSpeakerLabel = null;
+      SpeakerLabel previousSpeakerLabel = null;
+      Alternative alternative = null;
+
+      for (SpeakerLabel speakerLabel : transcription.getSpeakerLabels()) {
+        if (startSpeakerLabel == null) {
+          startSpeakerLabel = speakerLabel;
+          alternative = transcription.getAlternativeStartingAt(speakerLabel.getFrom());
+          previousSpeakerLabel = speakerLabel;
+          continue;
+        }
+
+        if (speakerLabel.getFrom() > alternative.getEndTime()) {
+          writer.write("Speaker " + startSpeakerLabel.getSpeaker() + " (" + startSpeakerLabel.getFrom() + "-" + previousSpeakerLabel.getTo() + "):");
+          writer.newLine();
+          writer.write(alternative.getTranscript());
+          writer.newLine();
+
+          startSpeakerLabel = speakerLabel;
+          alternative = transcription.getAlternativeStartingAt(speakerLabel.getFrom());
+        }
+
+        previousSpeakerLabel = speakerLabel;
+      }
+
+      writer.write("Speaker " + startSpeakerLabel.getSpeaker() + " (" + startSpeakerLabel.getFrom() + "-" + previousSpeakerLabel.getTo() + "):");
+      writer.newLine();
+      writer.write(alternative.getTranscript());
+      writer.newLine();
+    }
+
+    // keywords
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT3_FILENAME))) {
+      Map<String, Map<Integer, Integer>> keywordSpeakerCounts = new HashMap<>();
+
+      for (Result result : transcription.getResults()) {
+        for (Alternative alternative : result.getAlternatives()) {
+          for (Timestamp timestamp : alternative.getTimestamps()) {
+            if (timestamp.getWord().matches("[%].*")) {
+              String keyword = timestamp.getWord().substring(1);
+
+              int speakerIndex = transcription.getSpeakerIndex(timestamp);
+
+              if (keywordSpeakerCounts.containsKey(keyword)) {
+                if (keywordSpeakerCounts.get(keyword).containsKey(speakerIndex)) {
+                  keywordSpeakerCounts.get(keyword).put(speakerIndex, keywordSpeakerCounts.get(keyword).get(speakerIndex) + 1);
+                } else {
+                  keywordSpeakerCounts.get(keyword).put(speakerIndex, 1);
+                }
+              } else {
+                Map<Integer, Integer> speakerCounts = new HashMap();
+                speakerCounts.put(speakerIndex, 1);
+
+                keywordSpeakerCounts.put(keyword, speakerCounts);
+              }
+            }
+          }
+        }
+      }
+
+      for (String keyword : keywordSpeakerCounts.keySet()) {
+        for (Integer speakerIndex : keywordSpeakerCounts.get(keyword).keySet()) {
+          writer.write(keyword + ": Speaker " + speakerIndex + ": " + keywordSpeakerCounts.get(keyword).get(speakerIndex) + " time(s)");
+          writer.newLine();
+        }
+      }
+    }
+  }
+}
